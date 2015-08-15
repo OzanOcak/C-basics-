a78aa8717e4ba61f852cf998329e989b8af66460
Ozan Ocak <ozanocak@gmail.com>
2015-08-15 16:15:44 -0400

    fundementals  of ref and pointers in C++

--- project.cpp
+++ project.cpp
@@ -1,42 +1,40 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+
 
 using namespace std;
 
-int main(){
-   cout <<"hello C++"<<endl;
-   
-   string s="today is saturday";
-   
-   ofstream writer("output.txt");
-   
-   if(!writer){
-       cout<<"error opening file"<<endl;
-       return -1;
-    
-    }else{
-       writer<< s <<endl;
-       
-       writer.close();
-    }
-    
-    char letter;
-    
-    ifstream reader("output.txt");
-    
-       if(!reader){
-       cout<<"error opening file"<<endl;
-       return -1;
-    
-    }else{ 
-    
-      for(int i=0; ! reader.eof(); i++)
-       { 
-         reader.get(letter);
-         cout<<letter;
-       }
-       cout<<endl;
-       reader.close();
-    }
+void timeMachine(int* year)
+{
+cout<<"it used to be "<< *year <<endl;
+*year=2023;
+
+}
+
+void turnToTime(int& year)
+{
+year=2015;
+}
+
+int main()
+{
+
+int theYear=2015;
+
+int* yearPointer= &theYear;
+
+cout<<"Address of the pointer : "<< yearPointer<<endl;
+
+cout<<"data at memory address : "<< *yearPointer<<endl;
+
+timeMachine(&theYear);
+
+cout<<"the year is " << theYear <<" right now"<<endl;
+
+int& yearRef = theYear;
+
+turnToTime(yearRef);
+
+cout<<"the year is again "<< yearRef<<endl;
+
+
 }
